83d4092e6f476a4b647b731fce7d6feb7d940a21
Santiago Monroy <daniel2215713@correo.uis.edu.co>
2026-02-27 07:52:31 -0500

    Se agregaron los demas archivos

---
+++ Practicas/Practica_1/GNURadio/PRUEBA_1_epy_block_0.py
@@ -0,0 +1,22 @@
+import numpy as np
+from gnuradio import gr
+
+class blk(gr.sync_block):
+    def __init__(self):
+        gr.sync_block.__init__(
+            self,
+            name='e_Diff',
+            in_sig=[np.float32],
+            out_sig=[np.float32]
+        )
+        self.x_prev = 0.0   # muestra anterior
+
+    def work(self, input_items, output_items):
+        x = input_items[0]
+        y = output_items[0]
+
+        for i in range(len(x)):
+            y[i] = x[i] - self.x_prev
+            self.x_prev = x[i]
+
+        return len(y)
